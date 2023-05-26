61779008159a3119ad25aec2d1e6933129936d17
Jakob Tormalm <jakob.tormalm@gmail.com>
2023-05-26 15:12:17 +0200

    Vercel Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
